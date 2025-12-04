37a1b5d1d5ef2a594e5976f27ca987b61088035d
Ryan Barnes <ryan.barnes@uptickhq.com>
2025-12-04 17:02:53 +1100

    refactor(utils): add grid utils

--- src/days/day_04.rs
+++ src/days/day_04.rs
@@ -1,49 +1,31 @@
-#[derive(PartialEq, Clone, Copy)]
-enum Thing {
-    Paper,
-    Nothing,
-}
-fn parse_line(line: &str) -> Vec<Thing> {
-    line.chars().map(|c| match c {
-        '@' => Thing::Paper,
-        _ => Thing::Nothing,
-    }).collect()
-}
+use crate::utils::grid::{parse_grid, Grid};
 
-fn remove_rolls(grid: &Vec<Vec<Thing>>) -> (usize, Vec<Vec<Thing>>) {
+fn remove_rolls(grid: &Grid) -> (usize, Grid) {
     let mut removed = 0;
-    let mut updated_grid = grid.clone();
+    let mut updated_grid = grid.data.clone();
 
-    let grid_size = grid.len() - 1;
 
-    for (y, line) in grid.iter().enumerate() {
+    for (y, line) in grid.data.iter().enumerate() {
         for (x, slot) in line.iter().enumerate() {
-            if slot != &Thing::Paper { continue; }
+            if slot != &'@' { continue; }
 
-            let nw = if y == 0 || x == 0 { 0 } else if grid[y - 1][x - 1] == Thing::Paper { 1 } else { 0 };
-            let n = if y == 0 { 0 } else if grid[y - 1][x] == Thing::Paper { 1 } else { 0 };
-            let ne = if y == 0 || x == grid_size { 0 } else if grid[y - 1][x + 1] == Thing::Paper { 1 } else { 0 };
-            let e = if x == grid_size { 0 } else if grid[y][x + 1] == Thing::Paper { 1 } else { 0 };
-            let se = if y == grid_size || x == grid_size { 0 } else if grid[y + 1][x + 1] == Thing::Paper { 1 } else { 0 };
-            let s = if y == grid_size { 0 } else if grid[y + 1][x] == Thing::Paper { 1 } else { 0 };
-            let sw = if y == grid_size || x == 0 { 0 } else if grid[y + 1][x - 1] == Thing::Paper { 1 } else { 0 };
-            let w = if x == 0 { 0 } else if grid[y][x - 1] == Thing::Paper { 1 } else { 0 };
+            let count = grid.count_adjacent(x, y, '@', true);
 
-            removed += if nw + n + ne + e + se + s + sw + w < 4 {
-                updated_grid[y][x] = Thing::Nothing;
+            removed += if count < 4 {
+                updated_grid[y][x] = '.';
                 1
             } else { 0 }
         }
     }
 
-    (removed, updated_grid)
+    (removed, Grid { data: updated_grid })
 }
 
 
 pub fn exec(input: &str) -> (usize, usize) {
     let mut part_1 = 0;
     let mut part_2 = 0;
-    let mut grid = input.lines().map(parse_line).collect::<Vec<_>>();
+    let mut grid = parse_grid(input);
 
     loop {
         let (removed, updated_grid) = remove_rolls(&grid);
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,2 +1,3 @@
 pub mod file;
 pub mod itertools;
+pub mod grid;
