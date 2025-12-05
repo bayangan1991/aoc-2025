9bb0c1f6ee583fd0a23969e6dc6b11cdd166bf0b
Ryan Barnes <ryan.barnes@uptickhq.com>
2025-12-05 18:11:45 +1100

    refactor(main): use correct numbers

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,17 @@ mod utils;
 
 fn main() {
     let result = [
-        || (0, days::day_01::exec(&read_input("01"))),
-        || (1, days::day_02::exec(&read_input("02"))),
-        || (2, days::day_03::exec(&read_input("03"))),
-        || (3, days::day_04::exec(&read_input("04"))),
-        || (4, days::day_05::exec(&read_input("05"))),
+        || (1, days::day_01::exec(&read_input("01"))),
+        || (2, days::day_02::exec(&read_input("02"))),
+        || (3, days::day_03::exec(&read_input("03"))),
+        || (4, days::day_04::exec(&read_input("04"))),
+        || (5, days::day_05::exec(&read_input("05"))),
     ]
     .par_iter()
     .map(|f| f())
     .collect::<Vec<_>>();
 
     for result in result {
-        println!("Day {}: {:?}", result.0 + 1, result.1);
+        println!("Day {}: {:?}", result.0, result.1);
     }
 }
