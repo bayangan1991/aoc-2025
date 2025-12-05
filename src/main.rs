0a8fc39fbf5678422f49397368806ae328b3eb59
Ryan Barnes <ryan.barnes@uptickhq.com>
2025-12-06 00:33:08 +1100

    refactor(main): better solution printing

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,18 @@ mod utils;
 
 fn main() {
     let result = [
-        || (1, days::day_01::exec(&read_input("01"))),
-        || (2, days::day_02::exec(&read_input("02"))),
-        || (3, days::day_03::exec(&read_input("03"))),
-        || (4, days::day_04::exec(&read_input("04"))),
-        || (5, days::day_05::exec(&read_input("05"))),
+        || days::day_01::exec(&read_input("01")),
+        || days::day_02::exec(&read_input("02")),
+        || days::day_03::exec(&read_input("03")),
+        || days::day_04::exec(&read_input("04")),
+        || days::day_05::exec(&read_input("05")),
     ]
     .par_iter()
-    .map(|f| f())
+    .enumerate()
+    .map(|(index, f)| (index, f()))
     .collect::<Vec<_>>();
 
     for result in result {
-        println!("Day {}: {:?}", result.0, result.1);
+        println!("Day {}: {}\t{}", result.0 + 1, result.1.0, result.1.1);
     }
 }
